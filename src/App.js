23e1d0749205fb493e27c01390f25ca6681521c8
leicj <leicj@smartbow.net>
2017-12-31 11:56:18 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Button, Row } from 'antd';
 
 import KeyJump from './components/keyjump';
 import Bar from './components/bar';
+import CodeInOut from './components/codeinout';
 
 class App extends Component {
   state = {
@@ -15,15 +16,20 @@ class App extends Component {
       component = <KeyJump />
     } else if (type === 'bar') {
       component = <Bar />
+    } else if (type === 'codeinout') {
+      component = <CodeInOut />
     }
     return (
       <div className="App">
         <Row>
-          <Button type='primary' onClick={() => this.setState({ type: 'keyjump' })}>
-            字符跳动
-          </Button>
           <Button type='primary' onClick={() => this.setState({ type: 'bar' })}>
             柱状图
+          </Button>
+          <Button type='primary' onClick={() => this.setState({ type: 'codeinout' })}>
+            字符淡入淡出
+          </Button>
+          <Button type='primary' onClick={() => this.setState({ type: 'keyjump' })}>
+            字符跳动
           </Button>
         </Row>
         <Row>
---
+++ src/components/codeinout.js
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import * as d3 from 'd3';
+import { Row, Button } from 'antd';
+
+import './codeinout.css';
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+class CodeInOut extends React.Component {
+  componentDidMount() {
+    this.svg1();
+    this.svg2();
+    this.svg3();
+  }
+  svg1 = () => {
+    const svg = d3.select('.svg1');
+    const g = svg.append('g').attr('transform', `translate(32,100)`);
+
+    const update = (data) => {
+      const text = g.selectAll('text').data(data);
+      text.attr('class', 'update');
+
+      text.enter().append('text')
+        .attr('class', 'enter')
+        .attr('x', (d, i) => i * 32)
+        .attr('dy', '.35em')
+        .merge(text)
+        .text(d => d)
+      
+      text.exit().remove();
+    }
+
+    update(alphabet);
+
+    setInterval(() => {
+      update(d3.shuffle(alphabet).slice(0, Math.floor(Math.random() * 26)).sort());
+    }, 1500);
+  }
+  svg2 = () => {
+    const svg = d3.select('.svg2');
+    const g = svg.append('g').attr('transform', 'translate(32,100)');
+
+    const update = (data) => {
+      const text = g.selectAll('text').data(data, d => d);
+      text.attr('class', 'update');
+
+      text.enter().append('text')
+        .attr('class', 'enter')
+        .attr('dy', '.35em')
+        .merge(text)
+        .text(d => d)
+        .attr('x', (d, i) => i * 32)
+
+      text.exit().remove();
+    }
+
+    update(alphabet);
+
+    setInterval(() => {
+      update(d3.shuffle(alphabet).slice(0, Math.floor(Math.random() * 26)).sort());
+    }, 1500);
+  }
+  svg3 = () => {
+    const svg = d3.select('.svg3');
+    const g = svg.append('g').attr('transform', 'translate(32,100)');
+
+    const update = (data) => {
+      const text = g.selectAll('text').data(data, d => d);
+      const t = d3.transition().duration(750);
+
+      text.exit()
+        .attr('class', 'exit')
+        .transition(t)
+        .attr('y', 60)
+        .style('fill-opacity', 1e-6)
+        .remove();
+
+      text.attr('class', 'update')
+        .attr('y', 0)
+        .style('fill-opacity', 1)
+        .transition(t)
+        .attr('x', (d, i) => i * 32);
+
+      text.enter().append('text')
+        .attr('class', 'enter')
+        .attr('y', -60)
+        .attr('x', (d, i) => i * 32)
+        .style('fill-opacity', 1e-6)
+        .text(d => d)
+        .transition(t)
+        .attr('y', 0)
+        .style('fill-opacity', 1)
+    }
+
+    update(alphabet);
+
+    setInterval(() => {
+      update(d3.shuffle(alphabet).slice(0, Math.floor(Math.random() * 26)).sort());
+    }, 1500);
+  }
+  render() {
+    return (
+      <Row>
+        <svg width="960" height="200" className='svg1'></svg>
+        <svg width="960" height="200" className='svg2'></svg>
+        <svg width="960" height="200" className='svg3'></svg>
+      </Row>
+    )
+  }
+}
+
+export default CodeInOut;
+
